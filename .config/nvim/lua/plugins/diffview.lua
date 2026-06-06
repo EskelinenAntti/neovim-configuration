4033c2003aec9fe9c6e9aef5337810f292d23d9c
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-06-06 20:53:24 +0300

    Refresh Diffivew on focus gained

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -8,3 +8,11 @@ require("diffview").setup({
 vim.keymap.set("n", "<leader>g", function()
 	vim.cmd("DiffviewOpen")
 end, { desc = "Toggle Diffview" })
+
+vim.api.nvim_create_autocmd({ "FocusGained" }, {
+	callback = function()
+		if vim.fn.exists(":DiffviewRefresh") == 2 then
+			vim.cmd("silent! DiffviewRefresh")
+		end
+	end,
+})
